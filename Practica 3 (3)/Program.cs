00d75ecf2a0b6d4c80816435e85c56bc94f9c1bd
Arov752 <78989365+Arov752@users.noreply.github.com>
2021-02-18 13:51:47 -0400

    Add files via upload

---
+++ Practica 3 (3)/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Practica_3__3_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int a = 10 * 10;
+            Double b = 0;
+            int c = 0;
+            
+            while (c < 11)
+            {
+                if (b == 0.00)
+                {
+
+                    Console.WriteLine(a);
+                    
+                    b = a % 2;
+                    
+                }
+
+                a += 1;
+                c += 1;
+            }
+            Console.ReadKey();
+        }
+    }
+}
